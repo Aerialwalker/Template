bea639296292f42cab0ae86f43d239b4342302f4
Aerialwalker <zero720@live.com>
2020-07-23 13:50:45 +0800

    删除

--- moban/src/main/java/com/yc/template/Repository/AreaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yc.template.Repository;
-
-import com.yc.template.Entity.Area;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AreaRepository extends CrudRepository<Area,Long> {
-}
--- model/src/main/java/com/yc/model/Repository/AreaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yc.model.Repository;
-
-import com.yc.model.Entity.Area;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AreaRepository extends CrudRepository<Area,Long> {
-    Area findAllByArea_id();
-}
